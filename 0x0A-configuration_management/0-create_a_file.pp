04e8621f42490907142343d242c13df70ebad80d
mohamedellithyyyy <ellithy201102@gmail.com>
2024-06-29 04:09:46 +0300

    add 0x0A-configuration_management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
-# Create file in /tmp
+# creates a file in /tmp
 
-file { '/tmp/school' :
-    path    => '/tmp/school',
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet',
-    }
+file { '/tmp/school':
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
